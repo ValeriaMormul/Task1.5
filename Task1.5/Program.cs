82a46bb71d70fd3192b3bcaae71f8c435142acaf
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-01 23:47:49 +0200

    Добавьте файлы проекта.

---
+++ Task1.5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("10"); //Надання значення довжини прямокутника
+        double lenght = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("20"); //Надання значення ширини прямокутника
+        double width = double.Parse(Console.ReadLine());
+
+        double area = lenght + width;
+
+        Console.WriteLine($"Площа прямокутника: {area}");
+    }
+}
